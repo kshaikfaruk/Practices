d9a425bee03cf9bed5af62b10732095eee659341
shaikfaruk <shaikfaruk04@gmail.com>
2022-05-18 10:22:56 +0530

    update

--- src/partice/Factorialprograme.java
+++ src/partice/Factorialprograme.java
@@ -4,15 +4,13 @@ public class Factorialprograme {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    int fact=1;
-    int count=5;
-    for(int i=1;i<=count;i++){
-    	fact=fact*i;
-    	
-    	
-    }
-    System.out.println(fact);
-    
+      int fact=1;
+      int count=5;
+      for(int i=1;i<=count;i++){
+    	  fact=fact*i;
+    	  
+      }
+      System.out.println(fact);
 	}
 
 }
